f359e4b026b334c4cb08b810dffe8a21e7c7001c
Ramin B <i8ramin@gmail.com>
2016-10-10 15:29:04 -0400

    add logging

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -3,9 +3,13 @@ chrome.extension.sendMessage({}, function(response) {
     if (document.readyState === "complete") {
       clearInterval(readyStateCheckInterval);
 
+      console.info("[jira-link-externalizer] loaded...");
+
       delegate(document, 'a[href^="https://"]:not([href*="we.co"]):not([href*="atlassian.com"])', 'click', function(e) {
         var link = e.target;
         link.setAttribute('target', '_blank');
+
+        console.info("[jira-link-externalizer]", link);
       });
     }
   }, 10);
